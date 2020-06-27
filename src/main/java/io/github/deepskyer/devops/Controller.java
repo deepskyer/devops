b4136ce34c92b785960d2dd3bf64a6865a81c0a4
MS BING <you@example.com>
2020-06-27 16:34:06 -0500

    add a new spring boot project

---
+++ src/main/java/io/github/deepskyer/devops/Controller.java
@@ -0,0 +1,17 @@
+package io.github.deepskyer.devops;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public ResponseEntity<?> gethome(){
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+}
