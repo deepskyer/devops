afbfbf9e505a6126f46231646b1da3af6517b8cc
MS BING <you@example.com>
2020-06-27 18:52:02 -0500

    add a new spring boot project

--- src/main/java/io/github/deepskyer/devops/Controller.java
+++ src/main/java/io/github/deepskyer/devops/Controller.java
@@ -12,6 +12,6 @@ public class Controller {
     @RequestMapping("/")
     @ResponseBody
     public ResponseEntity<?> gethome(){
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+        return new ResponseEntity<>("Hello World from github", HttpStatus.OK);
     }
 }
