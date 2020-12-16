938e81de0e0870f7cebaeca63054ff1be4bf5a95
MS BING <you@example.com>
2020-12-15 20:20:44 -0600

    test1

--- src/main/java/io/github/deepskyer/devops/Controller.java
+++ src/main/java/io/github/deepskyer/devops/Controller.java
@@ -16,15 +16,7 @@ public class Controller {
     @ResponseBody
     public ResponseEntity<Map<String, String>> gethome(){
         Map<String, String> msg = new LinkedHashMap<>();
-        msg.put("Step0", "get all requirements running, including webhook");
-        msg.put("Step1", "git commit triggers CI build");
-        msg.put("Step2", "docker build your image");
-        msg.put("Step3", "docker push your image to a hub");
-        msg.put("Step4", "kubectl create deployment, service and ingress.");
-        msg.put("Step5", "kubectl apply -f deployment.yml");
-        msg.put("update Step1", "git commit change and build a new docker image");
-        msg.put("update Step2", "change to the new docker image version");
-        msg.put("update Step3", "kubectl apply -f deployment.yml");
+        msg.put("test", "1");
 
         return new ResponseEntity<>(msg, HttpStatus.OK);
     }
